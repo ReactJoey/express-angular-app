86b1c2c883bc83bb1803eae669ae46cf284d2789
J. M. Alessi <j.m.alessi27@outlook.com>
2022-06-18 22:42:24 -0400

    add user routes, express

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+/* These are the Express.js routes for users */
+
+const express = require('express');
+const router = express.Router();
+
+// Registration
+router.get('/register', (req, res, next) => {
+    res.send('Please register!');
+});
+
+// Authentication
+router.get('/auth', (req, res, next) => {
+    res.send('Please authenticate. :)');
+});
+
+// Profile
+router.get('/profile', (req, res, next) => {
+    res.send('Profile page.');
+});
+
+// Validation
+router.get('/validate', (req, res, next) => {
+    res.send('Validated, successfully. :)');
+});
+
+module.exports = router;
