3c6b0feda9f92e9847130407c21ca0dafa1d63b6
J. M. Alessi <j.m.alessi27@outlook.com>
2022-06-18 21:36:09 -0400

    express server setup + add routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+/* This is how I would teach juniors basic essentials. - J. A. */
+
+// To initialize express Router.
+var express = require('express');
+var router = express.Router();
+
+// To output this string to the root index route.
+router.get('/', function(req, res, next) {
+    res.send('Welcome to the index page.')
+});
+
+// To make it importable/usable in other files.
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,30 @@
+/* This is the default path for the project's Express.js backend which is written in Node.js by J. A. */
+
+// Initailizing Express.js
+var Express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// Initializing Server
+var app = express();
+
+// Views
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views');
+app.engine('html', require('ejs').renderFile);
+
+// Setting Client Static Folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Middleware for parsing through JSONs
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Generate URL extensions for client-side
+app.use('/', index);
+app.use('/api/', steps);
+
+// Listening to localhost port
+app.listen(port, function() {
+    console.log('listening on port '+port);
+};
