70f9501eb80704cbf5160610a48fd6731599f7a8
J. M. Alessi <j.m.alessi27@outlook.com>
2022-06-19 15:43:53 -0400

    bootswatch install + import

--- angular-client/src/main.ts
+++ angular-client/src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'bootswatch/dist/slate/bootstrap.min.css'; // for new UI style
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
