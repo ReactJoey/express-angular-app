b34e93135c7aa75b0003741b53b05ca1802ec901
J. M. Alessi <j.m.alessi27@outlook.com>
2022-06-19 22:53:29 -0400

    setup alerts, logout, redirect and add to navbar

--- angular-client/src/app/components/navbar/navbar.component.ts
+++ angular-client/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from 'express';
+import { AuthService } from 'src/app/services/auth.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+// Injected everything needed into the constructor
+  constructor(
+      private authService: AuthService,
+      private router: Router,
+      private flashMessage: FlashMessagesModule
+    ) { }
 
   ngOnInit(): void {
   }
+
+  // Added logout and success message
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessage.show('You have logged out successfully!', {
+      cssClass: 'alert-success', timeout: 3000
+    });
+    // Takes user back to login page after logout
+    this.router.nagivate(['/login']);
+    return false;
+  };
 
 }
