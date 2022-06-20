3d18881223336ae29181d0157d18bcf0f660cc7e
J. M. Alessi <j.m.alessi27@outlook.com>
2022-06-19 22:39:44 -0400

    inject constructors, login, register, auth setup

--- angular-client/src/app/components/login/login.component.ts
+++ angular-client/src/app/components/login/login.component.ts
@@ -12,11 +12,11 @@ export class LoginComponent implements OnInit {
   username: String | undefined;
   password: String | undefined;
   
-  // Inject AuthService, Router and FlashMessages into constructor.
+  // Inject AuthService, Router and Flash Messages into constructor.
   constructor(
     private authService: AuthService,
     private router: Router,
-    private flashMessages: FlashMessagesModule
+    private flashMessage: FlashMessagesModule
   ) { };
 
   ngOnInit(): void {
@@ -27,8 +27,19 @@ export class LoginComponent implements OnInit {
       username: this.username,
       password: this.password
     };
+    // If/Else to throw error for failure or to redirect to dashboard with success message if successful.
     this.authService.authenticateUser(user).subscribe(data => {
-      console.log(data);
-    });
-  }
+      if(data.success) {
+        this.authService.storeUserData(data.token, data.user);
+        this.flashMessage.show('You are now signed in.', {
+          cssClass: 'alert-success', timeout: 5000
+       });
+       this.router.navigate(['dashboard']);
+      } else {
+      this.flashMessage.show(data.message, {
+         cssClass: 'alert-danger', timeout: 5000
+      });
+      this.router.navigate(['login']);
+    };
+  });
 };
--- angular-client/src/app/services/auth.service.ts
+++ angular-client/src/app/services/auth.service.ts
@@ -4,25 +4,35 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
-    registerUser(user: { username: String; name: String; email: String; password: String; }) {
-        throw new Error('Method not implemented.');
-    }
-    authToken: String;
-    user: String;
-
+    user: any;
+    authToken: any | undefined;
     constructor(private http: HttpClient) { };
 
-    registerUser(user) {
+    registerUser(user: { username: String; name: String; email: String; password: String; }) {
+        authToken: String;
+        user: String;
         let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:3000/users/register', user, { headers: headers})
-            .map(response => response.json());
+        return this.http.post('http://localhost:3000/users/register', user, { headers: headers}).map((response: { json: () => any; }) => response.json());
     };
 
-    authenticateUser(user) {
+    authenticateUser(user: { username: String | undefined; password: String | undefined; }) {
         let headers = new HttpHeaders();
         headers.append('Content-Type', 'application/json');
         return this.http.post('http://localhost:3000/users/auth', user, { headers: headers })
-            .map(response => response.json());
+            .map((response: { json: () => any; }) => response.json());
+    };
+    // For storing user credentials including token, user name, name, email and password.
+    storeUserData(token: string, user: any) {
+        localStorage.setItem('id_token', token);
+        localStorage.setItem('user', JSON.stringify(user));
+        this.authToken = token;
+        this.user = user;
+    };
+    // Clears local storage cache of authToken upon logout.
+    logout() {
+        this.authToken = null;
+        this.user = null;
+        localStorage.clear();
     };
 };
