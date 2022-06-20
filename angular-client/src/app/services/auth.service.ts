af72a7c786ac63f72c0d2474c1061f79269ce6ee
J. M. Alessi <j.m.alessi27@outlook.com>
2022-06-20 00:53:27 -0400

    add getProfile() and loadToken() to auth.service

--- angular-client/src/app/services/auth.service.ts
+++ angular-client/src/app/services/auth.service.ts
@@ -22,6 +22,22 @@ export class AuthService {
         return this.http.post('http://localhost:3000/users/auth', user, { headers: headers })
             .map((response: { json: () => any; }) => response.json());
     };
+
+    getProfile() {
+        let headers = new HttpHeaders();
+        this.loadToken(); // to authenticate
+        headers.append('Authorization', this.authToken); // to add token to authorization header
+        headers.append('Content-Type', 'application/json');
+        return this.http.get('http://localhost:3000/users/profile', { headers: headers })
+            .map(response => response.json());
+    };
+
+    // Load token from local storage
+    loadToken() {
+        const token = localStorage.getItem('id_token');
+        this.authToken = token;
+    };
+
     // For storing user credentials including token, user name, name, email and password.
     storeUserData(token: string, user: any) {
         localStorage.setItem('id_token', token);
